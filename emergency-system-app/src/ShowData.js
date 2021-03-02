ad68a52fe7d4f0775ca98a8d9c884fe54c4c1da2
StefanutVlad <stefanut.tony@yahoo.com>
2021-03-03 01:01:27 +0200

    origin

--- emergency-system-app/src/ShowData.js
+++ emergency-system-app/src/ShowData.js
@@ -1,19 +1,35 @@
 import React, { useState } from "react";
+import "./ShowData.scss";
 
 function ShowData() {
   const [sensorData, setData] = useState([
     {
-      //.........
+      name: "Stef Vlad",
+      bpm: "55",
+      temperature: "36",
+      latitude: "47",
+      longitude: "23",
+      fall: "FALL DETECTED",
     },
     {
-      //........
+      name: "B B",
+      bpm: "82",
+      temperature: "34",
+      latitude: "42",
+      longitude: "19",
+      fall: "-",
     },
   ]);
+
   return (
     <div className="xxxx">
-      {sensorData.map((data) => {
-        // <h1>{data.name}</h1>
-      })}
+      {sensorData.map((user) => (
+        <div>
+          <h1>tt {user.bpm}</h1>
+          <br />
+          <h1> {user.name}</h1>
+        </div>
+      ))}
     </div>
   );
 }
